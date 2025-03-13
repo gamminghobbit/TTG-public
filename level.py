71d156bddaf4b463b17ea831113dbe0914645dfe
Eric Petersen <ericpetersen3.14@gmail.com>
2025-03-13 16:22:15 -0600

    game progress
    Co-authored-by: gamminghobbit <gamminghobbit@users.noreply.github.com>
    Co-authored-by: Shengjian Zhou <Isrejoy@users.noreply.github.com>
    Co-authored-by: Willdelf <Willdelf@users.noreply.github.com>

--- level.py
+++ level.py
@@ -1,8 +1,54 @@
 import pygame
 import time
+
+class Player:
+    def __init__(self):
+        # our player's absolute position
+        self.x = 0
+        self.y = 0
+        # our player's velocity
+        self.dx = 0
+        self.dy = 0
+    
+    # our position is changed based on our velocity
+    # Our current velocity is also changed by gravity
+    def update_position(self, elapsed_seconds):
+        velocity_scale = 0.01
+
+        gravity = 0 # 0.001 <- turn off gravity for right now
+        self.dy += gravity * elapsed_seconds
+
+        self.x += velocity_scale * self.dx * elapsed_seconds
+        self.y += velocity_scale * self.dy * elapsed_seconds
+    
+    # looks at the keys that are pressed and changes the velocity of the player
+    def update_velocity(self, elapsed_seconds):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.dy -= elapsed_seconds
+        elif keys[pygame.K_DOWN]:
+            self.dy += elapsed_seconds
+        
+        if keys[pygame.K_RIGHT]:
+            self.dx += elapsed_seconds
+        if keys[pygame.K_LEFT]:
+            self.dx -= elapsed_seconds
+
+    def cap_velocity(self):
+        self.dx = max(-1, min(1, self.dx))
+        self.dy = max(-1, min(1, self.dy))
+
+    # This will render our character - for now it's just a red circle
+    def render(self, screen):
+        center = (self.x, self.y)
+        pygame.draw.circle(screen, RED, center, 40)
+
 #colors
 RED =(255,0,0)
+
 def run(screen, start):
+    main_character = Player()
+
     background = pygame.image.load("images/background_1.png").convert()
     # rendering loop
     running = True
@@ -15,24 +61,27 @@ def run(screen, start):
         # clear the screen
         screen.fill((0, 0, 0))
 
+        # render the backdrop
         # render the level here
-        pygame.draw.circle(screen, RED, (200, 200), 100)
-        pygame.draw.polygon(screen, RED, [(200, 50), (100, 300), (300, 300)])
+        # render the character here
+        main_character.render(screen)
+
         # update the display
         pygame.display.flip()
-
-        # sleep for the right amount of time to make the game run at 60 fps
+        
+        # Update the position of the player
         elapsed = start - time.perf_counter()
-        fps_delay = max(0, (1 / 60) - elapsed)
-        time.sleep(fps_delay)
+        main_character.update_velocity(elapsed)
+        main_character.cap_velocity()
+        main_character.update_position(elapsed)
 
         """Controls!!!!"""
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             pass
-        if keys:[pygame.K_DOWN]:
+        if keys[pygame.K_DOWN]:
             pass
-        if keys:
+        if keys[pygame.K_LEFT]:
             pass
-        if keys:
+        if keys[pygame.K_RIGHT]:
             pass
